b9f8f0e89b174ff83100df56ce5ac82496c15f97
chanai <chanaiagwata82@gmail.com>
2023-02-09 00:09:34 +0300

    adds user class

---
+++ Password.py
@@ -0,0 +1,12 @@
+class User:
+    """
+    Class that generates new instances of users
+    """
+    def __init__(self, username, password):
+        """
+        __init__method that helps define properties of our objects
+        Args:
+            username: new user username
+            password: new user password
+        """
+        
