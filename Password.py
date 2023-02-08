e7f9ab2b0439a75db5196e046529c258471aa20e
chanai <chanaiagwata82@gmail.com>
2023-02-09 00:26:12 +0300

    adds credentials class

--- Password.py
+++ Password.py
@@ -2,6 +2,8 @@ class User:
     """
     Class that generates new instances of users
     """
+    user_list = [] #Empty user list
+
     def __init__(self, username, password):
         """
         __init__method that helps define properties of our objects
@@ -9,4 +11,18 @@ class User:
             username: new user username
             password: new user password
         """
+        self.username = username
+        self.password = password
+
+
+class Credentials:
+    """
+    Class that generates new instances of credentials
+    """
+    credentials_list  = [] #empty credential list
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
         
+
