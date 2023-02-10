23add9bfc7f51f46a64f78cdcf1b9626cf13d916
chanai <chanaiagwata82@gmail.com>
2023-02-10 23:57:25 +0300

    adds main method to run the application

--- run.py
+++ run.py
@@ -35,9 +35,13 @@ def display_credential():
     return Credentials.display_credentials()
 
 
+
 def main():
     print("Hello Welcome to your Password Locker. What is your name?")
     
     user_name = input()
 
     print(f"Hello {user_name}. What would you like to do?")
+
+    while True:  #Since True always evaluates to True, the loop will run indefinitely, until something within the loop returns or breaks.
+        print("Use the short codes : cu - create a new user account, cc - create new credential, sc - save credential,  dc - display credential, del - delete credential")
