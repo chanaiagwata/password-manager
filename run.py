0326be1d538d432d305b56f25fa89ef561483b5b
chanai <chanaiagwata82@gmail.com>
2023-02-10 23:18:27 +0300

    adds create,save, and delete methods

--- run.py
+++ run.py
@@ -1,2 +1,21 @@
 #!/usr/bin/env python3.10
 from Password import User, Credentials
+
+def create_user(username,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(username,password)
+    return new_user
+
+def save_credential(credential):
+    '''
+    Function to save contact
+    '''
+    credential.save_credential()
+
+def del_credential(credential):
+    '''
+    Function to delete a credential
+    '''
+    credential.delete_credential()
